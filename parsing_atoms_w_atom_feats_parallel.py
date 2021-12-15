96b40655ae84053248faae9c785e8fb5cd480013
Michael-C-Strobel <lidstromfan29@gmail.com>
2021-12-15 14:22:09 -0500

    Chaged atoms ids to atom indices.

--- parsing_atoms_w_atom_feats_parallel.py
+++ parsing_atoms_w_atom_feats_parallel.py
@@ -34,52 +34,82 @@ def process_system(filename):
     # filename_lst = [filename for filename in os.listdir(path_to_files) if 'site' in filename or 'protein' in filename]
     
     protein_w_H = mda.Universe(path_to_files + '/protein.mol2', format='mol2')
-    protein_w_H.ids = np.arange(0,len(protein_w_H.atoms))       # I'll be abusing the id attributes as by own settable index
+    # protein_w_H.ids = np.arange(0,len(protein_w_H.atoms))       # I'll be abusing the id attributes as by own settable index
+    # print(protein_w_H.ids)
+    # n_atoms = len(protein_w_H.atoms)
+    # the_ids = np.arange(0,len(protein_w_H.atoms)) 
+    # set_ids = protein_w_H.ids
 
     res_names = protein_w_H.residues.resnames
     new_names = [ "".join(re.findall("[a-zA-Z]+", name)).upper() for name in res_names]
     protein_w_H.residues.resnames = new_names
     
     protein_w_H = protein_w_H.select_atoms(selection_str)
+    # if np.max(protein_w_H.ids) >= n_atoms: 
+    #     raise Exception
+    indices_not_dropped = protein_w_H.indices
 
     # Calculate SAS for each atom, this needs to be done before hydrogens are dropped
-    traj = mdtrajload(path_to_files + '/protein.mol2')
-    SAS = shrake_rupley(traj, mode='atom')
-    if len(SAS) > 1:
-        # Sanity check, I'm pretty sure this should never happen
-        raise Exception("Did not expext more than one list of SAS values")   
-    SAS = SAS[0]
+    try:
+        traj = mdtrajload(path_to_files + '/protein.mol2')
+        SAS = shrake_rupley(traj, mode='atom')
+        if len(SAS) > 1:
+            # Sanity check, I'm pretty sure this should never happen
+            raise Exception("Did not expect more than one list of SAS values")   
+        # SAS_org = SAS
+        SAS = SAS[0]
+    except KeyError as e:
+        print("Value not included in dictionary \"{}\" while parsing {}.".format(e, path_to_files))
+        failed_list.append([path_to_files, "Value not included in dictionary \"{}\" while parsing {}.".format(e, path_to_files)])
+        return
 
-    mapping = defaultdict(lambda: -1)                           # A mapping from the old ids to the new ids
-    for i in range(len(protein_w_H.atoms.ids)):
-        mapping[protein_w_H.atoms.ids[i]] = i
-    original_ids = protein_w_H.ids
+    mapping = defaultdict(lambda: -1)                           # A mapping from atom indices to position in SAS
+    for i in range(len(protein_w_H.atoms)):
+        mapping[protein_w_H.atoms.indices[i]] = i
+
+    # if np.max(protein_w_H.ids) >= n_atoms: 
+    #     raise Exception
+    # original_ids = protein_w_H.ids
     # Keeping track of the ids is getting weird so I'm going to make a comment about it:
     # Every time we select atoms, we will need to know which atoms are saved and which get dropped
     # In this case, original_ids stores what atoms are left over after applying our selection string.
     # We'll use this to mask the mdtraj SAS output so we have the same atoms from each. We'll use it 
     # again later when we drop hydrogens
+    # if len(SAS) > len(temp):
+    #     raise Exception ("HOW")
+    # try:
+    #     SAS = [SAS[mapping[i]] for i in indices_not_dropped]                        # Sync SAS atoms with mda atoms
+    # except Exception as e:
+    #     # print("Max of org ids:", np.max(the_ids))
+    #     # print("Max of ids:", np.max(set_ids))
+    #     print("Index Error at:", filename)
+    #     print("Number of atoms read by mdtraj:", traj.n_atoms)
+    #     # print("Number of atoms read by MDA:", n_atoms)
+    #     raise e
 
-    SAS = [SAS[i] for i in original_ids]                        # Sync SAS atoms with mda atoms
-    protein_w_H.ids = np.arange(0,len(protein_w_H.atoms))       # Reset ids to contiguous values
+    # protein_w_H.ids = np.arange(0,len(protein_w_H.atoms))       # Reset ids to contiguous values
 
     # Add SAS from hydrogen to bonded atom, create number of bonded hydrogens feature
-    num_bonded_H = np.zeros(len(protein_w_H.atoms))
+    num_bonded_H = np.zeros(traj.n_atoms)
     for atom in protein_w_H:
         is_bonded_to_H = [re.search("^[a-zA-Z]+", bond.type).group().upper() == 'H' for _, bond in atom.bonds]
-        num_bonded_H[atom.id] = sum(is_bonded_to_H)
+        num_bonded_H[atom.index] = sum(is_bonded_to_H)
         # Because the bonds have the old ids, we use our map to the new ids to access the SAS value, if the value is -1
         # it means that the bonded atom no longer exists in our universe (i.e., it was dropped). If this happens it will
         # be a very rare occasion as must things other than solvents are not droppped.
-        local_SAS = np.array([SAS[mapping[atom_id[1]]] if mapping[atom_id[1]] != -1 else 0 for atom_id in atom.bonds.indices])    
-        SAS[atom.id] = np.sum(local_SAS * is_bonded_to_H)       # Only take the values from hydrogens
+        local_SAS = np.array([SAS[atom_idx[1]]  for atom_idx in atom.bonds.indices])    
+        SAS[atom.index] = np.sum(local_SAS * is_bonded_to_H)       # Only take the values from hydrogens
 
     # Drop Hydrogens
+    protein_w_H.ids = np.arange(0, len(protein_w_H.atoms))
     protein = protein_w_H.select_atoms("not type H")
-    original_ids = protein.ids                                  # Save the ids so we know what got dropped
-    protein.ids = np.arange(0,len(protein.atoms))               # Reset ids to contiguous values
-    SAS = [SAS[i] for i in original_ids]                        # Remove Hydrogens From SAS
-    num_bonded_H = [num_bonded_H[i] for i in original_ids]      # Remove hydroges from count of bonded hydrogens to each atom
+    protein.ids = np.arange(0, len(protein.atoms))              # Abusing atoms ids to make them zero-indexed which makes our life easier
+
+    
+    # original_ids = protein.ids                                  # Save the ids so we know what got dropped
+    # protein.ids = np.arange(0,len(protein.atoms))               # Reset ids to contiguous values
+    # SAS = [SAS[i] for i in protein.atoms.indices]                        # Remove Hydrogens From SAS
+    # num_bonded_H = [num_bonded_H[i] for i in protein.atoms.indices]      # Remove hydroges from count of bonded hydrogens to each atom
 
     trimmed = scipy.sparse.lil_matrix((len(protein.atoms.positions), len(protein.atoms.positions)), dtype='float')
     get_distance_matrix(protein.atoms.positions, trimmed, 7)
@@ -101,9 +131,9 @@ def process_system(filename):
             r = bins[1:]
             g = n/(pi_4 * r ** 2)
             # Add feature vector with [residue level feats] [atom level feats] [rdf] [SAS] [num bonded hydrogens per atom]
-            feature_array.append(np.concatenate((residue_properties.iloc[resname_dict[name]].values, atom_properties.iloc[atom_dict[element]].values, g, [SAS[atom.id]], [num_bonded_H[atom.id]])))                              # Add corresponding features to feature array
+            feature_array.append(np.concatenate((residue_properties.iloc[resname_dict[name]].values, atom_properties.iloc[atom_dict[element]].values, g, [SAS[atom.index]], [num_bonded_H[atom.index]])))                              # Add corresponding features to feature array
         except Exception as e:
-            print("Failed at atom:", atom)
+            print("Value not included in dictionary \"{}\" while parsing {}.".format(name, path_to_files))
             failed_list.append([path_to_files, "Value not included in dictionary \"{}\" while parsing {}.".format(name, path_to_files)])
             flag = True
             return
@@ -178,7 +208,7 @@ if not os.path.isdir('./data_atoms_w_atom_feats'):
 # Comment me out to run just one file
 num_cores = 8
 if __name__ == "__main__":
-    Parallel(n_jobs=num_cores)(delayed(process_system)(i,) for i in tqdm(inputs[1752+2448:]))
+    Parallel(n_jobs=num_cores)(delayed(process_system)(i,) for i in tqdm(inputs[1752+2448+64:]))
 
 np.savez('./failed_list', np.array(failed_list))
 ##########################################
