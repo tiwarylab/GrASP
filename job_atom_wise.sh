7e0fd186419f8b7bb23a276b3914ee365824e441
Michael-C-Strobel <lidstromfan29@gmail.com>
2022-06-22 13:06:46 -0400

    Update training slurm script.

--- job_atom_wise.sh
+++ job_atom_wise.sh
@@ -8,11 +8,12 @@
 #SBATCH --mail-user=strobelm@umd.edu
 #SBATCH --mail-type=ALL
 
+## train.py args node_noise_variance, ['cv', 'train_full', 'chen', 'coach420', 'holo4k', 'sc6k']
 
 module load anaconda3
 conda activate # source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load cuda/10.2
 conda activate ../../pytorch_env
-torchrun train.py 0.02
+torchrun train.py 0.02 holo4k
 
 
