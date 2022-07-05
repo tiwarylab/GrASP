355a6fcf8743760e23626ca1933281885c9d54e7
Michael Strobel <strobelm@br011.ib.bridges2.psc.edu>
2022-07-05 13:24:09 -0400

    Make featurizing atom file more specific.

--- parsing_regular.py
+++ parsing_regular.py
@@ -209,7 +209,7 @@ def process_system(path_to_protein_mol2_files, save_directory='./data_dir'):
             # Add feature vector with                                                  54-63          64               65               66               67-68        69-70                   71        72        73-79       80-81    82-83     84-85       86-87
             feature_array.append(np.concatenate((residue_dict[name], atom_dict[element], g, [SAS[atom.index]], formal_charge, num_bonds_w_heavy_atoms, is_in_ring, is_aromatic, num_radical_electrons, mass, hybridization, acceptor, donor, hydrophobe, lumped_hydrophobe)))  #,formal_charge     25                       # Add corresponding features to feature array
         except Exception as e:
-            print("Error while feautrizing atom for file {}.".format(path_to_files), flush=True)
+            print("Error while feautrizing atom for file {}.{}".format(path_to_files,e), flush=True)
             return -2
             # raise ValueError ("Value not included in dictionary \"{}\" while generating feature vector for {}.".format(name, path_to_files)) from e
 
