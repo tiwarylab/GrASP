7e19d594a01d690aae107d429cfeee1a0165c08f
Michael Strobel <strobelm@br011.ib.bridges2.psc.edu>
2022-07-05 14:03:58 -0400

    Change environment path in slurm script.

--- slurm_scripts/hyperparameter_slurm_scripts/job_train_4_heads.sh
+++ slurm_scripts/hyperparameter_slurm_scripts/job_train_4_heads.sh
@@ -13,7 +13,7 @@
 module load anaconda3
 conda activate # source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load cuda/10.2
-conda activate ../../pytorch_env
+conda activate envs/pytorch_env
 python train.py 0.02 cv 4
 
 
