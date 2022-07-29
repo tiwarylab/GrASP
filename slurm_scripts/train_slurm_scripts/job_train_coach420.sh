4c06836503131dcb42c1049b366df4d98d65ffd2
zwsmith200 <zwsmith200@gmail.com>
2022-07-29 11:29:30 -0400

    Training with best regularization

--- slurm_scripts/train_slurm_scripts/job_train_coach420.sh
+++ slurm_scripts/train_slurm_scripts/job_train_coach420.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -p GPU-shared
-#SBATCH -t 24:00:00
+#SBATCH -t 12:00:00
 #SBATCH --gpus=v100-32:4
 #SBATCH --ntasks-per-node=8
-#SBATCH --job-name="scPDB GPU coach420"
-#SBATCH --mail-user=strobelm@umd.edu
+#SBATCH --job-name="scPDB GPU coach420 Training"
+#SBATCH --mail-user=zsmith7@umd.edu
 #SBATCH --mail-type=ALL
-
-## train.py args node_noise_variance, ['cv', 'train_full', 'chen', 'coach420', 'holo4k', 'sc6k']
 
 module load anaconda3
 conda activate # source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load cuda/10.2
-conda activate envs/pytorch_env
-python3 train.py 0.02 coach420 Hybrid_1g12_self_edges
-
-
+conda activate pytorch_env
+python3 train.py -m hybrid -s coach420 -ls 0.2 -cw 0.8 1.2
--- slurm_scripts/train_slurm_scripts/job_train_cv.sh
+++ slurm_scripts/train_slurm_scripts/job_train_cv.sh
@@ -8,12 +8,10 @@
 #SBATCH --mail-user=zsmith7@umd.edu
 #SBATCH --mail-type=ALL
 
-## train.py args node_noise_variance, ['cv', 'train_full', 'chen', 'coach420', 'holo4k', 'sc6k']
-
 module load anaconda3
 conda activate # source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load cuda/10.2
 conda activate pytorch_env
-python3 train.py -m hybrid
+python3 train.py -m hybrid -ls 0.2 -cw 0.8 1.2
 
 
