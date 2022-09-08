381b27017f696371307bb47af32177dd78e0f84f
zwsmith200 <zwsmith200@gmail.com>
2022-09-08 14:55:27 -0400

    Slurm update

--- slurm_scripts/test_inference_slurm_scripts/job_test_site_metrics_coach420_mlig.sh
+++ slurm_scripts/test_inference_slurm_scripts/job_test_site_metrics_coach420_mlig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=15
-#SBATCH -p RM-small
+#SBATCH --ntasks-per-node=24
+#SBATCH -p RM-shared
 #SBATCH -t 1:00:00
 #SBATCH --job-name="scPDB Metrics Community Detection coach420_mlig"
 #SBATCH --mail-user=zsmith7@umd.edu
@@ -12,4 +12,4 @@ module load anaconda3
 conda activate # source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load cuda/10.2
 conda activate ob 
-python3 multisite_metrics_quantile_test.py coach420_mlig coach420_mlig/trained_model_m_transformer_gn_s_coach420_mlig_1661814669.7841315/epoch_49 -c louvain -d 4 -p .4 -a square
+python3 multisite_metrics_quantile_test.py coach420_mlig coach420_mlig/trained_model_m_transformer_gn_s_coach420_mlig_1661814669.7841315/epoch_49 -c louvain -d 5 -p .4 -a square -n 0
--- slurm_scripts/test_inference_slurm_scripts/michael_job_test_site_metrics_coach420_mlig.sh
+++ slurm_scripts/test_inference_slurm_scripts/michael_job_test_site_metrics_coach420_mlig.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=15
 #SBATCH -p RM-small
 #SBATCH -t 1:00:00
-#SBATCH --job-name="scPDB Metrics Community Detection holo4k"
+#SBATCH --job-name="scPDB Metrics Community Detection coach420_mlig"
 #SBATCH --mail-user=strobelm@umd.edu
 #SBATCH --mail-type=ALL
 
@@ -11,6 +11,5 @@
 module load anaconda3
 conda activate # source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load cuda/10.2
-conda activate ../../pytorch_env
-python3 multisite_metrics_quantile_test.py coach420 coach420/trained_model_1655980641.2358317/epoch_49 label
-
+conda activate ob 
+python3 multisite_metrics_quantile_test.py coach420_mlig coach420_mlig/trained_model_m_transformer_gn_s_coach420_mlig_1661814669.7841315/epoch_49 -c louvain -d 5 -p .4 -a square -n 2
