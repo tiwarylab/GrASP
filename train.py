3f9a35d9c7f4583b7dd1a098c2c503f2b79ffdff
zwsmith200 <zwsmith200@gmail.com>
2022-09-08 16:43:12 -0400

    Added missing sigmoid in validation

--- train.py
+++ train.py
@@ -297,6 +297,7 @@ def main(node_noise_variance : float, training_split='cv'):
                         unperturbed_x = torch.cat([data.x.clone().detach().to(device) for data in batch])
                         for data in batch:
                             data.x = data.x + (node_noise_variance**0.5)*torch.randn_like(data.x)
+                            data.y = distance_sigmoid(data.y, label_midpoint, label_slope)
                         labels  = torch.cat([data.y.clone().detach() for data in batch]).cpu().numpy()
                         y       = torch.cat([data.y.to(device) for data in batch])
                     
