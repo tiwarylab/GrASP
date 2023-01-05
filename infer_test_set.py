082cc227494ffb3321e1c3b2d692c28f19b1f8f1
zwsmith200 <zwsmith200@gmail.com>
2023-01-05 16:43:39 -0500

    Renamed simple_models.py

--- infer_test_set.py
+++ infer_test_set.py
@@ -21,8 +21,7 @@ from torch.utils.tensorboard import SummaryWriter
 import torch
 
 from GASP_dataset import GASPData
-from atom_wise_models import GASPformer_BN, GASPformer_GN, GASPformer_IN, GASPformer_IN_stats, GASPformer_PN, GASPformer_GNS, GASPformer_AON, GASPformer_no_norm
-from simple_models import GAT_model
+from model import GAT_model
 
 
 ###################################################################################
