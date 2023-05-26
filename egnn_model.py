fc01173a8a5cd03239a03e849010e09dfa0618c9
zwsmith200 <zwsmith200@gmail.com>
2023-05-26 13:43:34 -0400

    Combined clustering and segmentation

--- egnn_model.py
+++ egnn_model.py
@@ -11,18 +11,18 @@ from torch_geometric.nn import GATConv, GATv2Conv
 Equivariant updates follow the orignal paper, latent updates use GASP."""
 
 class EquivariantUpdate(nn.Module):
-    def __init__(self, hidden_nf, normalization_factor, aggregation_method,
+    def __init__(self, hidden_dim, normalization_factor, aggregation_method,
                  edges_in_d=1, act_fn=nn.SiLU(), tanh=False, coords_range=10.0):
         super(EquivariantUpdate, self).__init__()
         self.tanh = tanh
         self.coords_range = coords_range
-        input_edge = hidden_nf * 2 + edges_in_d
-        layer = nn.Linear(hidden_nf, 1, bias=False)
+        input_edge = hidden_dim * 2 + edges_in_d
+        layer = nn.Linear(hidden_dim, 1, bias=False)
         torch.nn.init.xavier_uniform_(layer.weight, gain=0.001)
         self.coord_mlp = nn.Sequential(
-            nn.Linear(input_edge, hidden_nf),
+            nn.Linear(input_edge, hidden_dim),
             act_fn,
-            nn.Linear(hidden_nf, hidden_nf),
+            nn.Linear(hidden_dim, hidden_dim),
             act_fn,
             layer)
         self.normalization_factor = normalization_factor
@@ -57,12 +57,12 @@ class EquivariantUpdate(nn.Module):
 
 
 class EquivariantBlock(nn.Module):
-    def __init__(self, hidden_nf, edge_feat_nf=7, act_fn=nn.SiLU(), n_layers=1,
+    def __init__(self, hidden_dim, edge_feat_nf=7, act_fn=nn.SiLU(), n_layers=1,
                  norm_diff=True, tanh=False, coords_range=15, norm_constant=1, sin_embedding=None,
                  normalization_factor=100, egnn_aggr='sum', GAT_aggr='multi', GAT_heads=4,
                  GAT_drop_prob=0.1, GAT_fill_value=torch.Tensor([0,0,0,0,0,0,1]), GAT_style=GATv2Conv):
         super(EquivariantBlock, self).__init__()
-        self.hidden_nf = hidden_nf
+        self.hidden_dim = hidden_dim
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.n_layers = n_layers
         self.coords_range_layer = float(coords_range)
@@ -78,10 +78,10 @@ class EquivariantBlock(nn.Module):
 
         # GASP Blocks
         for i in range(0, n_layers):
-            self.add_module("gasp_%d" % i, GAT_block(hidden_nf, hidden_nf, GAT_heads, edge_dim=edge_feat_nf, drop_prob=GAT_drop_prob,
+            self.add_module("gasp_%d" % i, GAT_block(hidden_dim, hidden_dim, GAT_heads, edge_dim=edge_feat_nf, drop_prob=GAT_drop_prob,
                                                      GAT_aggr=self.GAT_aggr, GAT_fill_value=self.GAT_fill_value,
                                                      GAT_style=self.GAT_style))
-        self.add_module("gcl_equiv", EquivariantUpdate(hidden_nf, edges_in_d=edge_feat_nf+1, act_fn=act_fn, tanh=tanh,
+        self.add_module("gcl_equiv", EquivariantUpdate(hidden_dim, edges_in_d=edge_feat_nf+1, act_fn=act_fn, tanh=tanh,
                                                        coords_range=self.coords_range_layer,
                                                        normalization_factor=self.normalization_factor,
                                                        aggregation_method=self.egnn_aggr))
@@ -103,13 +103,13 @@ class EquivariantBlock(nn.Module):
         return h, x
 
 
-class EGNN(nn.Module):
-    def __init__(self, in_node_nf, hidden_nf=64, act_fn=nn.SiLU(), n_layers=4,
-                 norm_diff=True, tanh=False, coords_range=15, norm_constant=1, inv_sublayers=1,
-                 sin_embedding=False, normalization_factor=100, egnn_aggr='sum', GAT_aggr='multi', GAT_heads=4,
-                 GAT_drop_prob=0.1, GAT_fill_value=torch.Tensor([0,0,0,0,0,0,1]), GAT_style=GATv2Conv):
-        super(EGNN, self).__init__()
-        self.hidden_nf = hidden_nf
+class GASP_EGNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim=64, output_dim=2, n_layers=12, GAT_heads=4,
+                 GAT_drop_prob=0.1, GAT_aggr='multi', GAT_fill_value=torch.Tensor([0,0,0,0,0,0,1]), 
+                 GAT_style=GATv2Conv, act_fn=nn.SiLU(), norm_diff=True, tanh=False, coords_range=15, 
+                 norm_constant=1, inv_sublayers=1, sin_embedding=False, normalization_factor=100, egnn_aggr='sum'):
+        super(GASP_EGNN, self).__init__()
+        self.hidden_dim = hidden_dim
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.n_layers = n_layers
         self.coords_range_layer = float(coords_range/n_layers)
@@ -127,42 +127,83 @@ class EGNN(nn.Module):
             edge_feat_nf = self.sin_embedding.dim * 2
         else:
             self.sin_embedding = None
-            edge_feat_nf = self.len(GAT_fill_value)
+            edge_feat_nf = len(self.GAT_fill_value)
 
         # GASP Encoder
         self.encoder = nn.Sequential( 
-            InstanceNorm(in_node_nf, affine=True),
-            nn.Linear(in_node_nf, self.hidden_nf, bias=False),
-            InstanceNorm(self.hidden_nf, affine=True),
+            InstanceNorm(input_dim, affine=True),
+            nn.Linear(input_dim, self.hidden_dim, bias=False),
+            InstanceNorm(self.hidden_dim, affine=True),
             nn.ELU(),
-            nn.Linear(self.hidden_nf, self.hidden_nf, bias=False),
-            InstanceNorm(self.hidden_nf, affine=True),
+            nn.Linear(self.hidden_dim, self.hidden_dim, bias=False),
+            InstanceNorm(self.hidden_dim, affine=True),
             nn.ELU()
         )
 
         for i in range(0, n_layers):
-            self.add_module("e_block_%d" % i, EquivariantBlock(hidden_nf, edge_feat_nf=edge_feat_nf,
+            self.add_module("e_block_%d" % i, EquivariantBlock(hidden_dim, edge_feat_nf=edge_feat_nf,
                                                                act_fn=act_fn, n_layers=inv_sublayers,
                                                                norm_diff=norm_diff, tanh=tanh,
                                                                coords_range=coords_range, norm_constant=norm_constant,
                                                                sin_embedding=self.sin_embedding,
                                                                normalization_factor=self.normalization_factor,
                                                                egnn_aggr=self.egnn_aggr, GAT_aggr=self.GAT_aggr,
-                                                               GAT_heads=self.GAT_heads, GAT_drop_prob=self.drop_prob,
+                                                               GAT_heads=self.GAT_heads, GAT_drop_prob=self.GAT_drop_prob,
                                                                GAT_fill_value=self.GAT_fill_value, 
                                                                GAT_style=self.GAT_style))
+
+        cat_dim = hidden_dim * (n_layers + 1)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(cat_dim, 4*hidden_dim),
+            nn.ELU(),
+            nn.Linear(4*hidden_dim, 2*hidden_dim),
+            nn.ELU(),
+            nn.Linear(2*hidden_dim, hidden_dim),
+            nn.ELU(),
+            nn.Linear(hidden_dim, int(hidden_dim/2)),
+            nn.ELU(),
+            nn.Linear(int(hidden_dim/2), int(hidden_dim/4)),
+            nn.ELU(),
+            nn.Linear(int(hidden_dim/4), output_dim)
+        )
+        
+        self.recon = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ELU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ELU(),
+            nn.Linear(hidden_dim, input_dim)
+        )
+
         self.to(self.device)
 
-    def forward(self, h, x, edge_index, edge_attr, node_mask=None, edge_mask=None, update_coords_mask=None):
+    def forward(self, input, node_mask=None, edge_mask=None, update_coords_mask=None):
+        h = input.x
+        x = input.coords
+        edge_index = input.edge_index
+        edge_attr = input.edge_attr
+        
         h = self.encoder(h)
+        jk_inputs = [h]
+
         for i in range(0, self.n_layers):
             h, x = self._modules["e_block_%d" % i](
                 h, x, edge_index, edge_attr=edge_attr, node_mask=node_mask, 
                 edge_mask=edge_mask, update_coords_mask=update_coords_mask)
+            jk_inputs.append(h)
+
+        message_out = h
+
+        h = torch.cat(jk_inputs, dim=-1)
+        h = self.decoder(h)
+
+        reconstruction = self.recon(message_out)
 
         if node_mask is not None:
             h = h * node_mask
-        return h, x
+            
+        return h, x, reconstruction
 
 
 class SinusoidsEmbeddingNew(nn.Module):
