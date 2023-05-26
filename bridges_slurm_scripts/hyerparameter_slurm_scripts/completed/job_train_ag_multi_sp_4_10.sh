a93c47a4347f78ecbedf082147bc2dcb8a289d71
zwsmith200 <zwsmith200@gmail.com>
2023-05-26 13:42:19 -0400

    Slurm scripts update

--- bridges_slurm_scripts/hyerparameter_slurm_scripts/completed/job_train_ag_multi_sp_4_10.sh
+++ bridges_slurm_scripts/hyerparameter_slurm_scripts/completed/job_train_ag_multi_sp_4_10.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 8:00:00
 #SBATCH --gpus=v100-32:4
 #SBATCH --ntasks-per-node=8
-#SBATCH --job-name="scPDB -s holo4k_mlig -ag multi"
+#SBATCH --job-name="scPDB -ag multi -sp 4 10"
 #SBATCH --mail-user=zsmith7@umd.edu
 #SBATCH --mail-type=ALL
 
@@ -13,6 +13,6 @@ module load anaconda3
 conda activate # source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load cuda/11.7.1
 conda activate pytorch_env
-python3 train.py -s holo4k_mlig -ag multi
+python3 train.py -ag multi -sp 4 10
 
 
--- bridges_slurm_scripts/hyerparameter_slurm_scripts/job_train_egnn.sh
+++ bridges_slurm_scripts/hyerparameter_slurm_scripts/job_train_egnn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -p GPU-shared
-#SBATCH -t 8:00:00
+#SBATCH -t 12:00:00
 #SBATCH --gpus=v100-32:4
 #SBATCH --ntasks-per-node=8
-#SBATCH --job-name="scPDB -s holo4k_mlig -ag multi"
+#SBATCH --job-name="train_egnn"
 #SBATCH --mail-user=zsmith7@umd.edu
 #SBATCH --mail-type=ALL
 
@@ -12,7 +12,8 @@
 module load anaconda3
 conda activate # source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load cuda/11.7.1
+module load cudnn/8.0.4
 conda activate pytorch_env
-python3 train.py -s holo4k_mlig -ag multi
+python3 train_egnn.py 
 
 
